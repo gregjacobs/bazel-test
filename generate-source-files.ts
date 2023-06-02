78c8c3576281c90ac4fcf36acd923c5a281ecae1
Gregory Jacobs <greg@greg-jacobs.com>
2023-06-01 23:37:56 -0500

    WIP package generator

--- generate-source-files.ts
+++ generate-source-files.ts
@@ -1,87 +1,119 @@
 import dedent from 'dedent';
 import fse from 'fs-extra';
 
-// Generate 10 packages that run sequentially (each relies on the previous)
-for (let i = 1; i <= 10; i++) {
-    const packageName = `package-with-dep-${i}`;
+// Generate 10 "root" packages
+for (let packageNum = 1; packageNum <= 10; packageNum++) {
+    const packageName = `package-${packageNum}`;
     const packageFolder = `packages/${packageName}`;
 
     // BUILD, package.json, tsconfig.json
-    const extraDeps = (i === 1) ? '' : `deps = ['//packages/package-with-dep-${i - 1}:library']`
-    generateSupportFiles(packageName, extraDeps);
+    const extraDeps = (packageNum === 1) ? '' : `deps = ['//packages/package-with-dep-${packageNum - 1}:library']`
+    generateSupportFiles({ packageName });
 
     // Output a source file with a change
     fse.outputFileSync(`${packageFolder}/src/index.ts`, dedent`
         import { doThing0 } from './functions';
-        ${i === 1 
-            ? '' 
-            : `export * from 'package-with-dep-${i - 1}';`
-        }
 
         console.log('update #${Date.now()}');
         console.log(doThing0);
 
-        export function myFn${i}() {
-            console.log('myFn${i}');
+        export function myFn${packageNum}() {
+            console.log('myFn${packageNum}');
         }
     `);
 
     // Output a source file with long file contents
-    fse.outputFileSync(`${packageFolder}/src/functions.ts`, generateLongSourceFile(i));
+    fse.outputFileSync(`${packageFolder}/src/functions.ts`, generateLongSourceFile(packageNum));
 }
 
 
-// Generate 100 packages that run in parallel
-for (let i = 1; i <= 100; i++) {
-    const packageName = `package-${i}`;
-    const packageFolder = `packages/${packageName}`;
-    // fse.removeSync(packageFolder);
 
-    // BUILD, package.json, tsconfig.json
-    generateSupportFiles(packageName);
+// // Generate 10 packages that run sequentially (each relies on the previous)
+// for (let i = 1; i <= 10; i++) {
+//     const packageName = `package-with-dep-${i}`;
+//     const packageFolder = `packages/${packageName}`;
 
-    // Output a source file with a change
-    fse.outputFileSync(
-        `${packageFolder}/src/index.ts`,
-        dedent`
-            import { doThing0 } from './functions';
+//     // BUILD, package.json, tsconfig.json
+//     generateSupportFiles(packageName);
 
-            console.log('update #${Date.now()}');
-            console.log(doThing0);
+//     // Output a source file with a change
+//     fse.outputFileSync(`${packageFolder}/src/index.ts`, dedent`
+//         import { doThing0 } from './functions';
+//         ${i === 1 
+//             ? '' 
+//             : `export * from 'package-with-dep-${i - 1}';`
+//         }
 
-            export function myFn${i}() {
-                console.log('myFn${i}');
-            }
-        `
-    );
+//         console.log('update #${Date.now()}');
+//         console.log(doThing0);
 
-    // Output a source file with long file contents
-    fse.outputFileSync(`${packageFolder}/src/functions.ts`, generateLongSourceFile(i));
-}
+//         export function myFn${i}() {
+//             console.log('myFn${i}');
+//         }
+//     `);
 
-// Generate a package that consumes all 100 parallel packages
-{
-    const packageName = `package-that-consumes-100`;
-    const packageFolder = `packages/${packageName}`;
+//     // Output a source file with long file contents
+//     fse.outputFileSync(`${packageFolder}/src/functions.ts`, generateLongSourceFile(i));
+// }
 
-    // BUILD, package.json, tsconfig.json
-    const deps: string[] = [];
-    for (let i = 1; i <= 100; i++) {
-        deps.push(`//packages/package-${i}:library`);
-    }
-    const extraDeps = `deps = ['${deps.join(`', '`)}']`;
-    generateSupportFiles(packageName, extraDeps);
 
-    // Output a source file with a change
-    fse.outputFileSync(`${packageFolder}/src/index.ts`, dedent`
-        import { myFn100 } from 'package-100';
+// // Generate 100 packages that run in parallel
+// for (let i = 1; i <= 100; i++) {
+//     const packageName = `package-${i}`;
+//     const packageFolder = `packages/${packageName}`;
+//     // fse.removeSync(packageFolder);
 
-        console.log('update #${Date.now()}');
-        console.log(myFn100);
-    `);
-}
+//     // BUILD, package.json, tsconfig.json
+//     generateSupportFiles(packageName);
 
-function generateSupportFiles(packageName: string, tsProjectDeps: string = '') {
+//     // Output a source file with a change
+//     fse.outputFileSync(
+//         `${packageFolder}/src/index.ts`,
+//         dedent`
+//             import { doThing0 } from './functions';
+
+//             console.log('update #${Date.now()}');
+//             console.log(doThing0);
+
+//             export function myFn${i}() {
+//                 console.log('myFn${i}');
+//             }
+//         `
+//     );
+
+//     // Output a source file with long file contents
+//     fse.outputFileSync(`${packageFolder}/src/functions.ts`, generateLongSourceFile(i));
+// }
+
+// // Generate a package that consumes all 100 parallel packages
+// {
+//     const packageName = `package-that-consumes-100`;
+//     const packageFolder = `packages/${packageName}`;
+
+//     // BUILD, package.json, tsconfig.json
+//     const deps: string[] = [];
+//     for (let i = 1; i <= 100; i++) {
+//         deps.push(`//packages/package-${i}:library`);
+//     }
+//     const extraDeps = `deps = ['${deps.join(`', '`)}']`;
+//     generateSupportFiles(packageName, extraDeps);
+
+//     // Output a source file with a change
+//     fse.outputFileSync(`${packageFolder}/src/index.ts`, dedent`
+//         import { myFn100 } from 'package-100';
+
+//         console.log('update #${Date.now()}');
+//         console.log(myFn100);
+//     `);
+// }
+
+/**
+ * Generates package.json, BUILD, and tsconfig.json files
+ */
+function generateSupportFiles({ packageName, tsProjectDeps }: {
+    packageName: string, 
+    tsProjectDeps: string = ''
+}) {
     const packageFolder = `packages/${packageName}`;
 
     // package.json
@@ -90,6 +122,7 @@ function generateSupportFiles(packageName: string, tsProjectDeps: string = '') {
         dedent`
             {
                 "name": "${packageName}",
+                "version": "0.0.0",
                 "main": "dist/index.js",
                 "typings": "dist/index.d.ts"
             }
@@ -100,8 +133,30 @@ function generateSupportFiles(packageName: string, tsProjectDeps: string = '') {
     fse.outputFileSync(
         `${packageFolder}/BUILD`,
         dedent`
+            load("@aspect_rules_js//js:defs.bzl", "js_library")
+            load("@aspect_rules_js//npm:defs.bzl", "npm_package")
             load("@aspect_rules_ts//ts:defs.bzl", "ts_config", "ts_project")
-
+            load("@npm//:defs.bzl", "npm_link_all_packages")
+            
+            npm_link_all_packages()
+            
+            npm_package(
+                name = "${packageName}",
+                srcs = [":lib"],
+                out = "pkg",
+                include_runfiles = False,
+                package = "${packageName}",
+                visibility = ["//visibility:public"],
+            )
+            
+            js_library(
+                name = "lib",
+                srcs = [
+                    "package.json",
+                    ":library"
+                ],
+            )
+            
             ts_project(
                 name = "library",
                 visibility = ['//visibility:public'],
@@ -115,7 +170,7 @@ function generateSupportFiles(packageName: string, tsProjectDeps: string = '') {
                 validate = False,
                 ${tsProjectDeps}
             )
-
+            
             ts_config(
                 name = "tsconfig",
                 src = "tsconfig.json",
@@ -138,12 +193,15 @@ function generateSupportFiles(packageName: string, tsProjectDeps: string = '') {
     );
 }
 
+/**
+ * Generates the contents of a long TypeScript source file
+ */
 function generateLongSourceFile(packageNum: number) {
     let fileContents = '';
-    for (let j = 0; j < 100000; j++) {
+    for (let j = 0; j < 20000; j++) {
         fileContents += `\
 export function doThing${j}() {
-    console.log('Hi ${packageNum} ${j}');
+    console.log('Hi ${packageNum} ${j} ${Math.random()}');
 }
 `;
     }
